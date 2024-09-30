79a9d1dd105b4da367838015ec7811754d795b20
LinaYQB <yqblina@gmail.com>
2024-09-30 22:09:28 +0200

    launching project

---
+++ src/MovieStore.ServiceLibrary/Domains/Movie.cs
@@ -0,0 +1,15 @@
+﻿using MovieStore.ServiceLibrary.Entities;
+using MovieStore.ServiceLibrary.Repositories;
+
+
+namespace MovieStore.ServiceLibrary.Domains
+{
+    public class Movie
+    {
+        public void SaveMovie(MovieEntity movieEntity)
+        {
+            var dataAccess = new MovieRepository();
+            dataAccess.SaveMovieToDatabase(movieEntity);
+        }
+    }
+}
---
+++ src/MovieStore.ServiceLibrary/Repositories/MovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using MovieStore.ServiceLibrary.Entities;
+
+
+namespace MovieStore.ServiceLibrary.Repositories
+{
+    public class MovieRepository
+    {
+        public void SaveMovieToDatabase(MovieEntity movieEntity)
+        {
+
+        }
+    }
+}
